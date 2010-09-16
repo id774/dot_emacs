4fc1c9e086ec65746bdd61f1fed4d6986f703b99
id774 <idnanashi@gmail.com>
2010-09-16 11:34:25 +0900

    Revert "#6 For Emacs 23.2 (CEDET included)"
    This reverts commit 88af8f8a0609c068b8fbf1030a31a0524c48b2fa.

--- dot_emacs.d/elisp/startup.el
+++ dot_emacs.d/elisp/startup.el
@@ -12,43 +12,21 @@
 (setenv "PATH" (concat '"/usr/local/bin:/opt/local/bin:/usr/bin:" (getenv "PATH")))
 
 ;;; my-load-path
-;;; Emacs 23.2 以降は CEDET 標準搭載のためロードパスを通さない
+;;; kaoru プロの設定を読んでかっこいいと思った。
 (defvar default-load-path load-path
   "*Base of `load-path'.
 It is used as a default value of target path to search file or
 subdirectory under load-path.")
-(cond
-  ((> emacs-major-version '22)
-    (cond
-      ((> emacs-minor-version '1)
-        (setq my-load-path
-          (list "/usr/local/share/emacs/site-lisp"
-          (expand-file-name "~/.emacs.d/elisp")
-          (expand-file-name "~/.emacs.d/elisp/3rd-party")
-          (expand-file-name "~/.emacs.d/elisp/3rd-party/jde/lisp")
-          (expand-file-name "~/.emacs.d/elisp/3rd-party/cedet/common")
-          (expand-file-name "~/.emacs.d/elisp/3rd-party/ecb")
-          (expand-file-name "~/.emacs.d/elisp/3rd-party/nxhtml")
-          (expand-file-name "~/.emacs.d/elisp/3rd-party/rhtml")
-          (expand-file-name "~/.emacs.d/elisp/3rd-party/rinari"))))
-        (setq my-load-path
-          (list "/usr/local/share/emacs/site-lisp"
-	        (expand-file-name "~/.emacs.d/elisp")
-	        (expand-file-name "~/.emacs.d/elisp/3rd-party")
-	        (expand-file-name "~/.emacs.d/elisp/3rd-party/jde/lisp")
-	        (expand-file-name "~/.emacs.d/elisp/3rd-party/ecb")
-	        (expand-file-name "~/.emacs.d/elisp/3rd-party/nxhtml")
-	        (expand-file-name "~/.emacs.d/elisp/3rd-party/rhtml")
-	        (expand-file-name "~/.emacs.d/elisp/3rd-party/rinari")))
-      (setq my-load-path
-        (list "/usr/local/share/emacs/site-lisp"
-        (expand-file-name "~/.emacs.d/elisp")
-        (expand-file-name "~/.emacs.d/elisp/3rd-party")
-        (expand-file-name "~/.emacs.d/elisp/3rd-party/jde/lisp")
-        (expand-file-name "~/.emacs.d/elisp/3rd-party/ecb")
-        (expand-file-name "~/.emacs.d/elisp/3rd-party/nxhtml")
-        (expand-file-name "~/.emacs.d/elisp/3rd-party/rhtml")
-        (expand-file-name "~/.emacs.d/elisp/3rd-party/rinari"))))))
+(setq my-load-path
+      (list "/usr/local/share/emacs/site-lisp"
+	    (expand-file-name "~/.emacs.d/elisp")
+	    (expand-file-name "~/.emacs.d/elisp/3rd-party")
+	    (expand-file-name "~/.emacs.d/elisp/3rd-party/jde/lisp")
+	    (expand-file-name "~/.emacs.d/elisp/3rd-party/cedet/common")
+	    (expand-file-name "~/.emacs.d/elisp/3rd-party/ecb")
+	    (expand-file-name "~/.emacs.d/elisp/3rd-party/nxhtml")
+	    (expand-file-name "~/.emacs.d/elisp/3rd-party/rhtml")
+	    (expand-file-name "~/.emacs.d/elisp/3rd-party/rinari")))
 (setq load-path (append my-load-path default-load-path))
 
 ;; custom
