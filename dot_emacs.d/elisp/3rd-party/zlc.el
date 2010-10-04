41c4a6018f379c40a310186c30f31b3acacd308a
id774 <idnanashi@gmail.com>
2010-10-04 09:48:24 +0900

    #13 Define C-n and C-p to cursor key of zlc

--- dot_emacs.d/elisp/3rd-party/zlc.el
+++ dot_emacs.d/elisp/3rd-party/zlc.el
@@ -171,6 +171,10 @@ select completion orderly."
 (let ((map minibuffer-local-map))
   (define-key map (kbd "<backtab>") 'zlc-select-previous)
   (define-key map (kbd "S-<tab>") 'zlc-select-previous)
+  (define-key map (kbd "C-p") 'zlc-select-previous)
+  (define-key map (kbd "C-n") 'zlc-select-next)
+  (define-key map (kbd "C-b") 'zlc-select-previous)
+  (define-key map (kbd "C-f") 'zlc-select-next)
   ;; (define-key map (kbd "C-c") 'zlc-reset)
   )
 
