df883f281362cc474c2c5178c27d7da2acbe5375
id774 <idnanashi@gmail.com>
2011-03-03 18:54:16 +0900

    #23 Refactoring view mode force exit

--- dot_emacs.d/elisp/view-mode-key.el
+++ dot_emacs.d/elisp/view-mode-key.el
@@ -53,17 +53,8 @@
            (not (file-directory-p file)))
       (view-file file)
     ad-do-it))
-;; 書き込み不能な場合はview-modeを抜けないように
-(defvar view-mode-force-exit nil)
-(defmacro do-not-exit-view-mode-unless-writable-advice (f)
-  `(defadvice ,f (around do-not-exit-view-mode-unless-writable activate)
-     (if (and (buffer-file-name)
-              (not view-mode-force-exit)
-              (not (file-writable-p (buffer-file-name))))
-         (message "File is unwritable, so stay in view-mode.")
-       ad-do-it)))
 
-(do-not-exit-view-mode-unless-writable-advice view-mode-exit)
-(do-not-exit-view-mode-unless-writable-advice view-mode-disable)
+;; 書き込み不能な場合はview-modeを抜けないように
+(viewer-stay-in-setup)
 
 (provide 'view-support)
