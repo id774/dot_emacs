b5f3aeb5e8b9557ae7fb8517fd221ead7f86cd4e
id774 <idnanashi@gmail.com>
2011-05-31 01:14:19 +0900

    #41 Fitting to 13-inch MacBook Pro

--- dot_emacs.d/elisp/faces.el
+++ dot_emacs.d/elisp/faces.el
@@ -91,8 +91,8 @@
               (setq default-frame-alist ; 13inch MacBook Pro に最適化
                     (append (list '(top . 0) ; 起動時の表示位置（上から）
                                   '(left . 0) ; 起動時の表示位置（左から）
-                                  '(width . 120) ; 起動時のサイズ（幅）
-                                  '(height . 40) ; 起動時のサイズ（縦）
+                                  '(width . 160) ; 起動時のサイズ（幅）
+                                  '(height . 45) ; 起動時のサイズ（縦）
                                   )))
               ;; (set-input-method "MacOSX")
               (setq fixed-width-use-QuickDraw-for-ascii t)
