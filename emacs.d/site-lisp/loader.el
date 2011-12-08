f0b006fd3f024208c0007c11f1965da39c0fde25
id774 <idnanashi@gmail.com>
2011-12-08 19:06:26 +0900

    63 Auto-install on proxy

--- emacs.d/site-lisp/loader.el
+++ emacs.d/site-lisp/loader.el
@@ -8,7 +8,10 @@
   (setq auto-install-directory "~/.emacs.d/site-lisp")
   (auto-install-update-emacswiki-package-name t)
   (auto-install-compatibility-setup)
-  (setq ediff-window-setup-function 'ediff-setup-windows-plain))
+  (setq ediff-window-setup-function 'ediff-setup-windows-plain)
+  (if global-proxy-use
+    (setq url-proxy-services 
+      '((concat ("http:" global-proxy-server ":" global-proxy-port))))))
 
 ;; Local Variables:
 ;; mode : emacs-lisp
