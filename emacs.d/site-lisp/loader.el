c44c5e834caf370028eccf7c169f38acd576e2e6
id774 <idnanashi@gmail.com>
2011-12-09 17:02:00 +0900

    65 Not loaded auto-install.el in proxy env

--- emacs.d/site-lisp/loader.el
+++ emacs.d/site-lisp/loader.el
@@ -3,15 +3,13 @@
 ;; After change this file, exec follow command.
 ;; $ emacs --batch -Q -f batch-byte-compile loader.el
 
+(if global-proxy-use ()
 ;; auto-install
-(when (load-p "auto-install")
-  (setq auto-install-directory "~/.emacs.d/site-lisp")
-  (auto-install-update-emacswiki-package-name t)
-  (auto-install-compatibility-setup)
-  (setq ediff-window-setup-function 'ediff-setup-windows-plain)
-  (if global-proxy-use
-    (setq url-proxy-services 
-      '((concat ("http:" global-proxy-server ":" global-proxy-port))))))
+  (when (load-p "auto-install")
+    (setq auto-install-directory "~/.emacs.d/site-lisp")
+    (auto-install-update-emacswiki-package-name t)
+    (auto-install-compatibility-setup)
+    (setq ediff-window-setup-function 'ediff-setup-windows-plain)))
 
 ;; Local Variables:
 ;; mode : emacs-lisp
