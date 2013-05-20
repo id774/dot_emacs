ae38ee732c2ea147bc43ae9a301e6453c1f79ff5
id774 <idnanashi@gmail.com>
2013-05-20 10:24:38 +0900

    115 Add sequential-command.el

--- emacs.d/elisp/autoloads.el
+++ emacs.d/elisp/autoloads.el
@@ -13,6 +13,17 @@
 (when (load-p "auto-complete")
   (global-auto-complete-mode t))
 
+;; sequential-command
+(when (require 'sequential-command-config)
+  (global-set-key "\C-a" 'seq-home)
+  (global-set-key "\C-e" 'seq-end)
+  (when (require 'org nil t)
+    (define-key org-mode-map "\C-a" 'org-seq-home)
+    (define-key org-mode-map "\C-e" 'org-seq-end))
+  (define-key esc-map "u" 'seq-upcase-backward-word)
+  (define-key esc-map "c" 'seq-capitalize-backward-word)
+  (define-key esc-map "l" 'seq-downcase-backward-word))
+
 ;; git.el
 (load-p "git")
 (load-p "git-blame")
