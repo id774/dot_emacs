ef09278fb8f6af9b3617a1f34fa7f2dee2ee3c69
id774 <idnanashi@gmail.com>
2013-11-27 09:16:05 +0900

    142 Remove smartchr for equal

--- emacs.d/elisp/autoloads.el
+++ emacs.d/elisp/autoloads.el
@@ -26,7 +26,6 @@
   (define-key esc-map "l" 'seq-downcase-backward-word))
 ;; smartchr
 (when (require 'smartchr)
-  (global-set-key (kbd "=") (smartchr '("=" " = " " == " " => ")))
   (global-set-key (kbd "{") (smartchr '("{" "{)" "{ `!!' }")))
   (global-set-key (kbd ">") (smartchr '(">" "=>" " => " " => '`!!''" " => \"`!!'\"")))
   (global-set-key (kbd "F") (smartchr '("F" "$" "$_" "$_->" "@$"))))
