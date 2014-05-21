2a250820a12ab886ca45ee0fac22a36db0336503
id774 <idnanashi@gmail.com>
2014-05-21 14:33:39 +0900

    172 Define platex2pdf command

--- emacs.d/elisp/yatex-mode.el
+++ emacs.d/elisp/yatex-mode.el
@@ -2,3 +2,10 @@
 (setq auto-mode-alist
   (cons (cons "\\.tex$" 'yatex-mode) auto-mode-alist))
 (autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+(setq tex-command "platex2pdf")
+(cond
+  ((eq system-type 'gnu/linux)
+    (setq dvi2-command "evince"))
+  ((eq system-type 'darwin)
+    (setq dvi2-command "open -a Preview")))
+(add-hook 'yatex-mode-hook '(lambda () (setq auto-fill-function nil)))
