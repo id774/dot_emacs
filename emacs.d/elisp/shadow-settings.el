7b843320d45e36523f7c7cfad983c0fd0dd24b40
id774 <idnanashi@gmail.com>
2014-08-15 00:35:24 +0900

    186 Using shadow.el only GNU/Linux platform

--- emacs.d/elisp/shadow-settings.el
+++ emacs.d/elisp/shadow-settings.el
@@ -1,4 +1,6 @@
-(when (load-p "shadow")
-  (add-hook 'find-file-hooks 'shadow-on-find-file)
-  (add-hook 'shadow-find-unshadow-hook
-    (lambda () (auto-revert-mode 1))))
+(cond
+  ((eq system-type 'gnu/linux)
+    (when (load-p "shadow")
+      (add-hook 'find-file-hooks 'shadow-on-find-file)
+      (add-hook 'shadow-find-unshadow-hook
+        (lambda () (auto-revert-mode 1))))))
