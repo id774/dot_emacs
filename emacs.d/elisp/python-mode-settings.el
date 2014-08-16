27c2e189e87b385bfd64dbbb3e64190c5ab27177
id774 <idnanashi@gmail.com>
2014-08-16 20:28:07 +0900

    190 Lisp for python.el of emacs24

--- emacs.d/elisp/python-mode-settings.el
+++ emacs.d/elisp/python-mode-settings.el
@@ -1,28 +1,47 @@
 ;; python-mode
 (cond
-  ((eq system-type 'gnu/linux)
-    ;; python
-    (when (autoload-p 'python-mode "python-mode" "Python editing mode." 'interactive)
+  ((>= emacs-major-version '24)
+    (progn
+      ;; python
+      (when (autoload 'python-mode "python-mode" "Python editing mode." t)
+        (setq python-mode-hook
+          (function (lambda ()
+            (local-set-key "\C-c\ p" 'python-pep8))))
+        (setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
+        (setq interpreter-mode-alist (cons '("python" . python-mode)
+                                           interpreter-mode-alist)))
+      ;; flymake for python
+      (add-hook 'python-mode-hook 'flymake-find-file-hook)
+      (when (load "flymake" t)
+        (defun flymake-pyflakes-init ()
+          (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                             'flymake-create-temp-inplace))
+                 (local-file (file-relative-name
+                              temp-file
+                              (file-name-directory buffer-file-name))))
+            (list "/usr/local/sbin/pyck"  (list local-file))))
+        (add-to-list 'flymake-allowed-file-name-masks
+                     '("\\.py\\'" flymake-pyflakes-init)))
+      (load-library "flymake-cursor")))
+  ((< emacs-major-version '24)
+    (progn
+      ;; python
       (setq python-mode-hook
         (function (lambda ()
           (local-set-key "\C-c\ p" 'python-pep8))))
       (setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
       (setq interpreter-mode-alist (cons '("python" . python-mode)
                                          interpreter-mode-alist)))
-    ;; flymake for python
-    (add-hook 'python-mode-hook 'flymake-find-file-hook)
-    (when (load "flymake" t)
-      (defun flymake-pyflakes-init ()
-        (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                           'flymake-create-temp-inplace))
-               (local-file (file-relative-name
-                            temp-file
-                            (file-name-directory buffer-file-name))))
-          (list "/usr/local/sbin/pyck"  (list local-file))))
-      (add-to-list 'flymake-allowed-file-name-masks
-                   '("\\.py\\'" flymake-pyflakes-init)))
-    (load-library "flymake-cursor"))
-  ((eq system-type 'darwin)
-    (setq python-mode-hook
-      (function (lambda ()
-        (local-set-key "\C-c\ p" 'python-pep8))))))
+      ;; flymake for python
+      (add-hook 'python-mode-hook 'flymake-find-file-hook)
+      (when (load "flymake" t)
+        (defun flymake-pyflakes-init ()
+          (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                             'flymake-create-temp-inplace))
+                 (local-file (file-relative-name
+                              temp-file
+                              (file-name-directory buffer-file-name))))
+            (list "/usr/local/sbin/pyck"  (list local-file))))
+        (add-to-list 'flymake-allowed-file-name-masks
+                     '("\\.py\\'" flymake-pyflakes-init)))
+      (load-library "flymake-cursor")))
