86e74668417f7807ef88d626a6197c567c552d2b
id774 <idnanashi@gmail.com>
2014-08-16 22:02:50 +0900

    191 Fix version specify problem

--- emacs.d/elisp/python-mode-settings.el
+++ emacs.d/elisp/python-mode-settings.el
@@ -1,6 +1,6 @@
 ;; python-mode
 (cond
-  ((>= emacs-major-version '24)
+  ((< emacs-major-version '24)
     (progn
       ;; python
       (when (autoload 'python-mode "python-mode" "Python editing mode." t)
@@ -23,7 +23,7 @@
         (add-to-list 'flymake-allowed-file-name-masks
                      '("\\.py\\'" flymake-pyflakes-init)))
       (load-library "flymake-cursor")))
-  ((< emacs-major-version '24)
+  ((>= emacs-major-version '24)
     (progn
       ;; python
       (setq python-mode-hook
@@ -32,16 +32,4 @@
       (setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
       (setq interpreter-mode-alist (cons '("python" . python-mode)
                                          interpreter-mode-alist)))
-      ;; flymake for python
-      (add-hook 'python-mode-hook 'flymake-find-file-hook)
-      (when (load "flymake" t)
-        (defun flymake-pyflakes-init ()
-          (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                             'flymake-create-temp-inplace))
-                 (local-file (file-relative-name
-                              temp-file
-                              (file-name-directory buffer-file-name))))
-            (list "/usr/local/sbin/pyck"  (list local-file))))
-        (add-to-list 'flymake-allowed-file-name-masks
-                     '("\\.py\\'" flymake-pyflakes-init)))
-      (load-library "flymake-cursor")))
+  ))
