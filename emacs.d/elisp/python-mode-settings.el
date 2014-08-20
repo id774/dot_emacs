404558a3bb4e92e8121fd7596ae4b7176582bff9
id774 <idnanashi@gmail.com>
2014-08-20 16:36:29 +0900

    195 Remove obsolete alist

--- emacs.d/elisp/python-mode-settings.el
+++ emacs.d/elisp/python-mode-settings.el
@@ -22,11 +22,7 @@
       (load-library "flymake-cursor")))
   ((>= emacs-major-version '24)
     (progn
-      ;; python
       (setq python-mode-hook
         (function (lambda ()
-          (local-set-key "\C-c\ p" 'python-pep8))))
-      (setq auto-mode-alist (cons '("\\.py$" . python-mode) auto-mode-alist))
-      (setq interpreter-mode-alist (cons '("python" . python-mode)
-                                         interpreter-mode-alist)))
+          (local-set-key "\C-c\ p" 'python-pep8)))))
   ))
