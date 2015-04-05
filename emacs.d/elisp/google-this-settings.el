9178c27e1d27874675c38fcd7fcf681d2054f597
id774 <idnanashi@gmail.com>
2015-04-06 00:09:22 +0900

    209 Add google-this.el

--- emacs.d/elisp/google-this-settings.el
+++ emacs.d/elisp/google-this-settings.el
@@ -3,4 +3,4 @@
 (defun google-this-url () "URL for google searches."
   (concat google-this-base-url google-this-location-suffix
           "/search?q=%s&hl=ja&num=10&as_qdr=y5&lr=lang_ja"))
-(global-set-key (kbd "M-g M-g") 'google-this)
+(global-set-key (kbd "M-g M-t") 'google-this)
