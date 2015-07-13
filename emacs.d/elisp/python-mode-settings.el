1bfc1960c0fdfd4cade1edf5de22c36009617ac9
id774 <idnanashi@gmail.com>
2015-07-13 13:25:37 +0900

    211 Using autopep8 with emacs

--- emacs.d/elisp/python-mode-settings.el
+++ emacs.d/elisp/python-mode-settings.el
@@ -4,13 +4,20 @@
 ;; Python Path
 (setq python-shell-interpreter "/opt/python/current/bin/python")
 
+;; for executable-find
+(setq exec-path (cons (expand-file-name "/opt/python/current/bin") exec-path))
+
 ;; python-mode
 (cond
   ((< emacs-major-version '24)
     (progn
       (setq python-mode-hook
         (function (lambda ()
-          (local-set-key "\C-c\ p" 'python-pep8))))
+          (local-set-key "\C-c\ p" 'python-pep8)
+          (require 'py-autopep8)
+          (define-key python-mode-map (kbd "C-c F") 'py-autopep8)        ; バッファ全体のコード整形
+          (define-key python-mode-map (kbd "C-c f") 'py-autopep8-region) ; 選択リジョン内のコード整形
+        )))
       (add-hook 'python-mode-hook 'flymake-find-file-hook)
       (when (load "flymake" t)
         (defun flymake-pyflakes-init ()
@@ -27,5 +34,9 @@
     (progn
       (setq python-mode-hook
         (function (lambda ()
-          (local-set-key "\C-c\ p" 'python-pep8)))))
+          (local-set-key "\C-c\ p" 'python-pep8)
+          (require 'py-autopep8)
+          (define-key python-mode-map (kbd "C-c F") 'py-autopep8)
+          (define-key python-mode-map (kbd "C-c f") 'py-autopep8-region)
+        ))))
   ))
