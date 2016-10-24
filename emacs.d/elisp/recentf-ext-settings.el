a55f63699d6bc0719b2093668d90fda1cbbe995b
id774 <idnanashi@gmail.com>
2016-10-24 12:40:14 +0900

    229 Add recentf-ext.el

--- emacs.d/elisp/recentf-ext-settings.el
+++ emacs.d/elisp/recentf-ext-settings.el
@@ -9,6 +9,6 @@
 (setq recentf-max-saved-items 9999)            ;; recentf に保存するファイルの数
 (setq recentf-exclude '(".recentf"))           ;; .recentf 自体は含まない
 (setq recentf-auto-cleanup 'never)             ;; 保存する内容を整理
-(run-with-idle-timer 600 t '(lambda ()         ;; 600 秒ごとに .recentf を保存
+(run-with-idle-timer 3600 t '(lambda ()        ;; 3600 秒ごとに .recentf を保存
    (with-suppressed-message (recentf-save-list))))
 (require 'recentf-ext)
