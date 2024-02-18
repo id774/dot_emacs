89cc64cee390a825174b92233d2811150f02cf40
id774 <idnanashi@gmail.com>
2024-02-18 11:15:03 +0900

    250 Fixed pyflakes integration in Emacs 23.4 for Python files by adjusting python-mode settings

--- emacs.d/elisp/python-mode-settings.el
+++ emacs.d/elisp/python-mode-settings.el
@@ -9,28 +9,7 @@
 
 ;; python-mode
 (cond
-  ((< emacs-major-version '24)
-    (progn
-      (setq python-mode-hook
-        (function (lambda ()
-          (local-set-key "\C-c\ p" 'python-pep8)
-          (require 'py-autopep8)
-          (define-key python-mode-map (kbd "C-c F") 'py-autopep8)        ; バッファ全体のコード整形
-          (define-key python-mode-map (kbd "C-c f") 'py-autopep8-region) ; 選択リジョン内のコード整形
-        )))
-      (add-hook 'python-mode-hook 'flymake-find-file-hook)
-      (when (load "flymake" t)
-        (defun flymake-pyflakes-init ()
-          (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                             'flymake-create-temp-inplace))
-                 (local-file (file-relative-name
-                              temp-file
-                              (file-name-directory buffer-file-name))))
-            (list "/usr/local/sbin/pyck"  (list local-file))))
-        (add-to-list 'flymake-allowed-file-name-masks
-                     '("\\.py\\'" flymake-pyflakes-init)))
-      (load-library "flymake-cursor")))
-  ((>= emacs-major-version '24)
+  ((>= emacs-major-version '23)
     (progn
       (setq python-mode-hook
         (function (lambda ()
